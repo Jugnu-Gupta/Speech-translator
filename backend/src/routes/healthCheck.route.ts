1425509edaf9da44c5f0332c07ff53db99dce3c4
jugnugupta33 <jugnugupta33@gmail.com>
2025-07-20 02:32:44 +0530

    first commit 2

--- backend/src/routes/healthCheck.route.ts
+++
@@ -1,8 +0,0 @@
-import { healthCheck } from "../controllers/healthCheck.controller";
-import { Router } from "express";
-const router = Router();
-
-// Health check route
-router.route("/").get(healthCheck);
-
-export default router;
